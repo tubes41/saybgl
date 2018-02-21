d9ab6b5e7b6cffce9359dbe604854e84a869e46f
tubes41 <2478421+tubes41@users.noreply.github.com>
2018-02-21 11:35:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,12 +5,12 @@ if (empty($_GET)){
   header("HTTP/1.1 200 OK");
   header('Content-Type:text/plain');
 
-  echo $_GET['nsurl'] . 'pebble?units=' . $_GET['unit'] . "\r\n";
+  echo "URL: " . $_GET['nsurl'] . 'pebble?units=' . $_GET['unit'] . "\r\n";
   $jsonResponse = file_get_contents($_GET['nsurl'] . 'pebble?units=' . $_GET['unit']);
   $jsonData = json_decode($jsonResponse);
 
-  echo $jsonResponse . "\r\n";
-  echo $jsonData . "\r\n";
+  echo "GET Response:" . $jsonResponse . "\r\n";
+  echo "Decoded:" . $jsonData . "\r\n";
 
   $myObj->timestamp = $jsonData->bgs->datetime;
   $myObj->age = 30;
@@ -18,6 +18,6 @@ if (empty($_GET)){
 
   $myJSON = json_encode($myObj);
 
-  echo $myJSON;
+  echo "My JSON:" . $myJSON;
 }
 ?>
