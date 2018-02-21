d75d2c7a0cf890fe97773017c699ec9c35e3c413
tubes41 <2478421+tubes41@users.noreply.github.com>
2018-02-21 13:31:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,22 @@
 <?php
 if (empty($_GET['nsurl']) || empty($_GET['unit'])){
   header("HTTP/1.1 400 Bad Request");
-	echo ($_SERVER['HTTP_nsurl']);
-	echo ($_SERVER['HTTP_unit']);
 } else {
-  header("HTTP/1.1 200 OK");
-  header('Content-Type:application/json');
-
   $jsonResponse = file_get_contents($_GET['nsurl'] . 'pebble?units=' . $_GET['unit']);
   $jsonData = json_decode($jsonResponse);
-
-$speech = "Your BGL reading as of " . date("g:ia",$jsonData->bgs[0]->datetime) . " is " . $jsonData->bgs[0]->sgv . " and trending " . $jsonData->bgs[0]->direction;
-
+  $speech = "Your BGL reading as of " . date("g:ia",$jsonData->bgs[0]->datetime) . " is " . $jsonData->bgs[0]->sgv . " and trending " . $jsonData->bgs[0]->direction;
 
   $myObj = (object)[
-	  'messages' => (array)[
-		  (object)[
-        'speech' 		=>  $speech,
-        'displayText'   =>  $speech
-			]
+	'messages' => (array)[
+		(object)[
+        		'speech' 	=>  $speech,
+        		'displayText'   =>  $speech
+		]
 	  ]
   ];
   $myJSON = json_encode($myObj);
+  header("HTTP/1.1 200 OK");
+  header('Content-Type:application/json');
   echo $myJSON;
 }
 ?>
