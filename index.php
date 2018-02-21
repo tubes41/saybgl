3786b13ba788da0355a87b571043c1c68cd13074
tubes41 <2478421+tubes41@users.noreply.github.com>
2018-02-21 15:02:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
-if (empty($_GET['nsurl']) || empty($_GET['unit'])){
+if (empty($_GET['nsurl']) || empty($_GET['unit']) || empty($_GET['TZ'])){
   header("HTTP/1.1 400 Bad Request");
 } else {
-  $jsonResponse = file_get_contents(urldecode($_GET['nsurl']) . 'pebble?units=' . urldecode($_GET['unit']));
-  $jsonData = json_decode($jsonResponse);
+	$jsonResponse = file_get_contents(urldecode($_GET['nsurl']) . 'pebble?units=' . urldecode($_GET['unit']));
+	$jsonData = json_decode($jsonResponse);
 	error_log("SGV: ". $jsonData->bgs[0]->sgv);
 	error_log("Trend Direction: ". $jsonData->bgs[0]->direction);
-  $speech = "Your BGL reading as of " . date("g:ia",$jsonData->bgs[0]->datetime) . " is " . $jsonData->bgs[0]->sgv . " and trending " . $jsonData->bgs[0]->direction;
+	date_default_timezone_set($_GET['TZ'])
 
-  $myObj = (object)[
-        'fulfillmentText' =>  $speech
-  ];
-  $myJSON = json_encode($myObj);
-  header("HTTP/1.1 200 OK");
-  header('Content-Type:application/json');
-  echo $myJSON;
+	$speech = "Your BGL reading as of " . date("g:ia",$jsonData->bgs[0]->datetime) . " is " . $jsonData->bgs[0]->sgv . " and trending " . $jsonData->bgs[0]->direction;
+
+	$myObj = (object)[
+	'fulfillmentText' =>  $speech
+	];
+	$myJSON = json_encode($myObj);
+	header("HTTP/1.1 200 OK");
+	header('Content-Type:application/json');
+	echo $myJSON;
 }
 ?>
