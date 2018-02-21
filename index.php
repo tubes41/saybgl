069267f2f188e254a57ffe7c9bfdba3acbaa9780
tubes41 <2478421+tubes41@users.noreply.github.com>
2018-02-21 11:22:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
 if (empty($_GET)){
-header("HTTP/1.1 400 Bad Request");
+  header("HTTP/1.1 400 Bad Request");
+} else {
+  $jsonResponse = file_get_contents($_GET['nsurl'] & 'pebble?units=' & $_GET['unit']);
+  $jsonData = json_decode($jsonResponse);
+
+  echo $_GET['nsurl'];
+
+  $myObj->timestamp = $jsonData->bgs->datetime;
+  $myObj->age = 30;
+  $myObj->city = "New York";
+
+  $myJSON = json_encode($myObj);
+
+  echo $myJSON;
 }
-$jsonResponse = file_get_contents($_GET['nsurl'] & 'pebble?units=' & $_GET['unit']);
-$jsonData = json_decode($jsonResponse);
-
-$myObj->timestamp = $jsonData->bgs->datetime;
-$myObj->age = 30;
-$myObj->city = "New York";
-
-$myJSON = json_encode($myObj);
-
-echo $myJSON;
 ?>
